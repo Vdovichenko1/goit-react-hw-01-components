4aa789e0ff5d5d4636859fad2384232e4ecb547f
Anton Vdovichenko <106983322+Vdovichenko1@users.noreply.github.com>
2022-10-24 10:56:48 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,19 @@ import user from "../user.json";
 import data from '../data.json';
 
 export const App = () => {
-  return <div>
-    <Profile />
-  </div>;
+  return <>
+    <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  followers={user.stats.followers}
+  views={user.stats.views}
+  likes={user.stats.likes}
+    />
+    <Statistics title="Upload stats" stats={data} />
+    <Statistics stats={data} />
+  </>;
 };
 
 
